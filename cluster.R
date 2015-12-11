8a5256891b3e277e2864c6a474cb74c346745b59
Thomas King <imthomasking+github@gmail.com>
2015-12-11 07:31:23 -0800

    Add clustering

---
+++ cluster.R
@@ -0,0 +1,33 @@
+# Libao Jin
+# December 11, 2015
+
+rm(list = ls())
+
+# import data from .xlsx file
+# install.packages("gdata")
+library("gdata")
+library("cluster")
+library("fpc")
+file <- "daisy.xlsx"
+data <- read.xls(file)
+std.data <- scale(data, center = TRUE, scale = TRUE)
+# print(std.data)
+
+d <- dist(std.data, method = "euclidean", diag = TRUE, upper = FALSE)
+hcs <- hclust(d, method = "complete")
+
+k <- kmeans(std.data, 2, iter.max = 10, nstart = 1, algorithm = "MacQueen", trace=FALSE)
+print(k)
+cluster <- k$cluster
+dev.new()
+pdf("kmeans_clustering_1.pdf")
+clusplot(std.data, cluster, color = TRUE, shade = TRUE, labels = 2, lines = 0)
+dev.off()
+dev.new()
+pdf("kmeans_clustering_2.pdf")
+plotcluster(std.data, cluster)
+dev.off()
+
+output_cluster <- "kmeans_cluster.csv"
+write.csv(cluster, output_cluster)
+
